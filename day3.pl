67e6563ffcd7e9b83c1f56fe1b59736aa4dc4494
sverzel <stefan@verzel.se>
2023-12-03 23:12:53 +0100

    Part two, day 3.

--- day3.pl
+++ day3.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 
 use strict;
-use List::Util qw(sum);
+use List::Util qw(sum product);
 
 my @g;
 {
@@ -11,10 +11,11 @@ my @g;
 }
 
 my @part_numbers;
+my $gear_ratio = 0;
 for (my $y = 0; $y < 140; $y++) {
     for (my $x = 0; $x < 140; $x++) {
 	if ($g[$y][$x] =~ /[^\d\.]/) {
-	    warn "Found '$g[$y][$x]' at $x, $y\n";
+	    my @gear_parts;
 	    foreach my $dy(-1 .. 1) {
 	      scope: foreach my $dx(-1 .. 1) {
 		  if ($g[$y+$dy][$x+$dx] =~ /\d/) {
@@ -22,14 +23,17 @@ for (my $y = 0; $y < 140; $y++) {
 		      my $n = '';
 		      1 while ($x+ --$s >= 0 && $g[$y+$dy][$x+$s] =~ /\d/);
 		      $n .= $g[$y+$dy][$x+$s] while ($x+ ++$s < 140 && $g[$y+$dy][$x+$s] =~ /\d/);
-		      warn "Found part number $n\n";
 		      push @part_numbers, $n;
+		      push @gear_parts, $n if $g[$y][$x] eq '*';
 		      last scope if $s > 0;
 		  }
 	      }
 	    }
+
+	    $gear_ratio += product @gear_parts if @gear_parts > 1;
 	}
     }
 }
 
 print "Sum of part numbers: ", sum(@part_numbers), "\n";
+print "Sum of all of the gear ratios: $gear_ratio\n";
