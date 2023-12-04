6237833ebe7e69e55cab60c0724dd6702e2a2cc2
sverzel <stefan@verzel.se>
2023-12-04 20:26:09 +0100

    Formatting.

--- day4.pl
+++ day4.pl
@@ -8,17 +8,17 @@ $input .= '_input';
 
 open my $fh, $input or die $!;
 
-my $s1 = 0;
+my $s = 0;
 my %cards;
 while (my $line = <$fh>) {
     my $p = sum(map $line =~ /^Card\s+\d+:\s.*\b$_\b.*\|/, grep /\d/, split /\s+/, join '', map $_, $line =~ /\|(.*)$/) || 0;
     
-    $s1 += ($p ? 2**($p-1) : 0);
+    $s += ($p ? 2**($p-1) : 0);
     
     my ($c) = $line =~ /^Card\s+(\d+):/;
     $cards{$c}++;
     $cards{$c+$_+1} += $cards{$c} for 0 .. $p-1;
 }
 
-print "Points in total: $s1\n";
+print "Points in total: $s\n";
 print "Total scratchcards: ", sum(map $cards{$_}, keys %cards), "\n";
