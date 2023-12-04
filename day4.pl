64c7cc855f18da163f66c9e2cfccda6cc82475ed
sverzel <stefan@verzel.se>
2023-12-04 17:17:04 +0100

    Day 4, part two.

--- day4.pl
+++ day4.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl -w
 
 use strict;
+use List::Util qw(sum);
 
 my ($input) = $0 =~ /^([^.]+)/;
 $input .= '_input';
@@ -8,13 +9,21 @@ $input .= '_input';
 open my $fh, $input or die $!;
 
 my $s1 = 0;
+my %cards;
 while (<$fh>) {
     my $p = 0;
     foreach my $t(grep /\d/, split /\s+/, join '', map $_, /\|(.*)$/) {
 	$p++ if /^Card\s+\d+:\s.*\b$t\b.*\|/;
     }
-
+    
     $s1 += ($p > 0 ? 2**($p-1) : 0);
+    
+    my ($c) = /^Card\s+(\d+):/;
+    $cards{$c}++;
+    for (my $n = 0; $n < $p; $n++) {
+	$cards{$c+$n+1} += $cards{$c};
+    }
 }
 
 print "Points in total: $s1\n";
+print "Total scratchcards: ", sum(map $cards{$_}, keys %cards), "\n";
